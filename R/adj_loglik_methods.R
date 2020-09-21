84ce7fc88224b98d278985ee166e4af8c47fcaa1
tbruckbauer <1592639+tbruckbauer@users.noreply.github.com>
2020-09-21 22:45:56 +0100

    start anova.chantrics
    + start anova.chantrics method
    + create model generics function checker & implement it for glms
    + housekeeping

--- R/adj_loglik_methods.R
+++ R/adj_loglik_methods.R
@@ -32,3 +32,44 @@ adj_loglik <- function(x,
   class(adjusted_x) <- c("chantrics", "chandwich", class(x))
   return(adjusted_x)
 }
+#' ANOVA tables: compare nested models
+#'
+#' \code{anova} method for \code{chantrics} objects
+#'
+#' Compare two or more nested models that have been adjusted using the
+#' \code{\link{adj_logLik}} method. It uses the adjusted likelihood ratio test
+#' statistic (ALRTS), as described in Section 3.5 of
+#' \href{http://doi.org/10.1093/biomet/asm015}{Chandler and Bate (2007)}.
+#'
+#' Note that the objects specified do not have to be sorted in a specific way,
+#' they will be sorted by the function by the number of parameters, as returned
+#' by \code{attr(model1, "p_current")}.
+#'
+#' @param model1 Object of class \code{chantrics}, as returned by
+#'   \code{\link{adj_logLik}}.
+#' @param model2 Object of class \code{chantrics}, as returned by
+#'   \code{\link{adj_logLik}}.
+#' @param ... Further objects of class \code{chantrics}, as returned by
+#'   \code{\link{adj_logLik}}, and/or parameters that will be passed to
+#'   \code{\link[chandwich]{anova.chandwich}}, and then further to
+#'   \code{\link[chandwich]{compare_models}}. The type of adjustment, out of
+#'   \code{"vertical"}, \code{"cholesky"}, \code{"spectral"}, \code{"none"}, as
+#'   specified in the parameter \code{type}, can also be specified here.
+#'
+#' @export
+anova.chantrics <- function(model1, model2, ...) {
+  dotargs <- list(...)
+  potential_model_objects <-
+    c(model1, model2, subset(dotargs, names(dotargs) == ""), use.names = FALSE)
+  #check whether the unnamed objects are actually chantrics objects
+  pmo_is_chantrics <- sapply(potential_model_objects, function(x){is(x, "chantrics")})
+  #warn user that we drop supplied unnamed arguments that are not chantrics objects
+  if(any(pmo_is_chantrics)){
+    warn("One or more of the unnamed objects supplied are not 'chantrics' objects,\nwhich have been dropped.")
+  }
+  #drop non-chantrics objects
+  model_objects <- subset(potential_model_objects, pmo_is_chantrics)
+  model_objects
+  #find out if sorting is necessary
+  #prepare for piping into chandwich::anova.chandwich
+}
--- tests/testthat/test-glm.R
+++ tests/testthat/test-glm.R
@@ -1,18 +1,30 @@
 context("Tests the aLogLik method and submethods associated with glm models.")
 
+#fit the misspecified poisson model from Introducing chandwich
+set.seed(123)
+x <- rnorm(250)
+y <- rnbinom(250, mu = exp(1 + x), size = 1)
+fm_pois <- glm(y ~ x + I(x ^ 2), family = poisson)
+#reference
+pois_glm_loglik <- function(pars, y, x) {
+  log_mu <- pars[1] + pars[2] * x + pars[3] * x ^ 2
+  return(dpois(y, lambda = exp(log_mu), log = TRUE))
+}
+reference_pois_logLik <- pois_glm_loglik(fm_pois$coefficients, y, x)
+chantrics_pois_logLik <- logLik_vec(fm_pois, fm_pois$coefficients)
 test_that("logLik_vec.glm() returns correct loglik-vector if passed the correct object",
-          {
-            #from Introducing Chandwich
-            set.seed(123)
-            x <- rnorm(250)
-            y <- rnbinom(250, mu = exp(1 + x), size = 1)
-            fm_pois <- glm(y ~ x + I(x ^ 2), family = poisson)
-            #reference
-            pois_glm_loglik <- function(pars, y, x) {
-              log_mu <- pars[1] + pars[2] * x + pars[3] * x ^ 2
-              return(dpois(y, lambda = exp(log_mu), log = TRUE))
-            }
-            expect_equal(c(pois_glm_loglik(fm_pois$coefficients, y, x)), unname(c(
-              logLik_vec(fm_pois, fm_pois$coefficients)
-            )))
+          {expect_equal(c(reference_pois_logLik), unname(c(chantrics_pois_logLik)))
           })
+
+# !! add unit tests for other glm link functions for logLik_vec() here !!
+
+#adjust fm_pois
+fm_pois_adj <- adj_loglik(fm_pois)
+
+# !! add unit tests for adj_loglik here !!
+
+test_that("Are generics accessible for adjusted glm models?", {
+  #test that there is no error
+  expect_error(model_generics_caller(fm_pois_adj), regexp = NA)
+})
+
