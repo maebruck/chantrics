2d0acb27dc8cddcb50c98a80d222079408c9b77b
tbruckbauer <1592639+tbruckbauer@users.noreply.github.com>
2021-03-17 12:12:31 +0000

    fix citation

--- R/chantrics.R
+++ R/chantrics.R
@@ -1,7 +1,8 @@
 #' chantrics: Loglikelihood Adjustments for Econometric Models
 #'
 #' `chantrics` adjusts the loglikelihood of common econometric models for
-#' clustered data based on the estimation process suggested in @chanbate07,
+#' clustered data based on the estimation process suggested in
+#' [Chandler and Bate (2007)](http://doi.org/10.1093/biomet/asm015),
 #' using the [chandwich](https://cran.r-project.org/package=chandwich) package,
 #' and provides convenience functions for inference on the adjusted models.
 #' `adj_loglik()` adjusts the parameter covariance matrix of the models to
@@ -14,6 +15,10 @@
 #'
 #' See \code{vignette("chantrics-vignette", package = "chantrics")} for an
 #' overview of the package.
+#'
+#' @references R. Chandler and S. Bate, Inference for clustered data using the
+#'   independence loglikelihood, Biometrika, 94 (2007), pp. 167–183.
+#'   <http://doi.org/10.1093/biomet/asm015>.
 #'
 #' @docType package
 #' @name chantrics
